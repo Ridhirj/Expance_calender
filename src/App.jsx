9551efd74f12d7f8e75355cabe4c57cfdbeb19b3
Ridhirj <ridhijain2412@gmail.com>
2023-07-13 23:48:53 +0530

    A react app is started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ExpenseItem from './components/ExpenseItem.jsx'
+import './App.css'
+function App() {
+  return (
+    <>
+      <div> 
+        <ExpenseItem></ExpenseItem>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
