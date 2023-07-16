00b25973e854f007e5d98835dc4837917243b4bb
Ridhirj <ridhijain2412@gmail.com>
2023-07-17 00:01:30 +0530

    basic structure is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import ExpenseItem from './components/ExpenseItem.jsx'
-import './App.css'
+import Expenses from "./components/Expenses/Expenses.jsx";
+
+import "./App.css";
 function App() {
+  const data = [
+    {
+      id: "e1",
+      title: "toilet paper",
+      amount: 2000,
+      date: new Date(2023, 6, 13),
+    },
+    {
+      id: "e2",
+      title: "party clothes",
+      amount: 7000,
+      date: new Date(2023, 5, 23),
+    },
+    {
+      id: "e3",
+      title: "stationary",
+      amount: 200,
+      date: new Date(2023, 8, 13),
+    },
+    {
+      id: "e4",
+      title: "milk",
+      amount: 250,
+      date: new Date(2023, 6, 20),
+    },
+  ];
   return (
     <>
-      <div> 
-        <ExpenseItem></ExpenseItem>
+      <div>
+        <Expenses items={data} />
       </div>
-       
     </>
-  )
+  );
 }
 
-export default App
+export default App;
